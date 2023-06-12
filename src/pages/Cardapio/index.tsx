89ec9c9eedb49d98706a0291455fce6eb8c25725
endreo123 <endreo.leoni@gmail.com>
2023-06-11 22:22:18 -0300

    aula 3

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { Buscador } from './Buscador';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo} from 'assets/logo.svg';
+import { Filtros } from './Filtros';
 
 export const Cardapio = () => {
     const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
 
     return (
         <main>
@@ -16,10 +18,16 @@ export const Cardapio = () => {
             </header>
             <section className={styles.cardapio}>
                 <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                    <Buscador 
+                <Buscador 
                     busca={busca}
                     setBusca={setBusca}
                 />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros 
+                        filtro={filtro}
+                        setFiltro={setFiltro}
+                    />
+                </div>
             </section>
         </main>
     )
